27763fc734beedb06f84802a9c299c86a273dc2e
Camila Solis <camila@MacBook-Air.local>
2020-09-16 23:42:55 -0300

    FEAT: the api was added and it is shown in layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: 'https://graphqlzero.almansi.me/api'
+});
+
+const UsersQuery = () => {
+  return <Query query={gql`{
+      users{
+        data{
+          id,
+          name,
+          email,
+          company{
+            name
+          }
+        }
+      }
+  }`}>
+    {({ loading, error, data }) => {
+      if(loading)return <p>Loading...</p>
+      if(error)return <p>Error!</p>
+
+      return data.users.data.map(user =>{
+      return <p key={user.id}>{user.name}</p>
+      })
+    }}
+  </Query>
+}
+
+function App() {
+  return <div className="App">
+    <UsersQuery />
+  </div>;
+}
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <ApolloProvider client={ client }>
+    <App/>
+  </ApolloProvider>,
+  rootElement
+)
