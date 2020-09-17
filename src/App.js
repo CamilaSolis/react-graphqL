f4c869ec179d155a53c723109b41cbb17e8a8eaa
Camila Solis <camila@MacBook-Air.local>
2020-09-17 01:00:48 -0300

    FIX: the way of reading data was changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+
+const READ_USER = gql`
+  query users{
+    users{
+      data{
+        id,
+        name,
+        email,
+        company{
+          name
+        }
+      }
+    }
+  }
+`;
+
+const CREATE_USER = gql`
+  mutation CreateUser($text: String!) {
+    createUser(text: $text)
+  }
+`;
+
+const REMOVE_USER = gql`
+  mutation RemoveUser($id: String!) {
+    removeUser(id: $id)
+  }
+`;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const { data, loading, error } = useQuery(READ_USER);
+
+  if(loading) return <p>Cargando data...</p>;
+  if(error) return <p>ERROR</p>;
+  if(!data) return <p>La data no se encuentra</p>;
+
+  return(
+    <div>
+      <h3>Crear nuevo usuario</h3>
+      {/* <form onSubmit={}>
+        <input type="text" placeholder="Ingrese nuevo usuario"></input>
+        <button type="submit">Enviar</button>
+      </form> */}
+      <ul>
+        {data.users.data.map((user) =>
+          <li key={user.id}>
+            <p>{user.name}</p>
+          </li>
+        )
+
+        }
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,49 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient, { gql } from 'apollo-boost';
-import { ApolloProvider, Query } from 'react-apollo';
-
 import './index.css';
+import App from './App';
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 
 const client = new ApolloClient({
   uri: 'https://graphqlzero.almansi.me/api'
 });
 
-const UsersQuery = () => {
-  return <Query query={gql`{
-      users{
-        data{
-          id,
-          name,
-          email,
-          company{
-            name
-          }
-        }
-      }
-  }`}>
-    {({ loading, error, data }) => {
-      if(loading)return <p>Loading...</p>
-      if(error)return <p>Error!</p>
-
-      return data.users.data.map(user =>{
-      return <p key={user.id}>{user.name}</p>
-      })
-    }}
-  </Query>
-}
-
-function App() {
-  return <div className="App">
-    <UsersQuery />
-  </div>;
-}
-
-const rootElement = document.getElementById("root");
-
 ReactDOM.render(
-  <ApolloProvider client={ client }>
-    <App/>
+  <ApolloProvider client={client}>
+    <App />
   </ApolloProvider>,
-  rootElement
-)
+  document.getElementById('root')
+);
